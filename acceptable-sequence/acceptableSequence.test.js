764f5b60b689fd485e23c9ca8f8d63e27f37cf29
Alexithemia <44597137+Alexithemia@users.noreply.github.com>
2018-12-04 21:52:21 -1000

    Add files via upload

---
+++ acceptable-sequence/acceptableSequence.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const acceptableSequence = require('../acceptableSequence');
+
+describe('Acceptable Sequence Test', () => {
+  it('should return true for "++a++===+b+=+c+==+"', () => {
+    const checkSequence = acceptableSequence('++a++===+b+=+c+==+');
+    assert.deepEqual(checkSequence, true);
+  });
+  it('should return false for "x++===+y+=+z+==+"', () => {
+    const checkSequence = acceptableSequence('x++===+y+=+z+==+');
+    assert.deepEqual(checkSequence, false);
+  });
+  it('should return false for "lmnop"', () => {
+    const checkSequence = acceptableSequence('lmnop');
+    assert.deepEqual(checkSequence, false);
+  });it('should return false for "lmnop"', () => {
+    const checkSequence = acceptableSequence('+x++===+y+=+z');
+    assert.deepEqual(checkSequence, false);
+  });
+  it('should return false for "lmnop"', () => {
+    const checkSequence = acceptableSequence('++a++===+b+=+c==+');
+    assert.deepEqual(checkSequence, false);
+  });
+})
